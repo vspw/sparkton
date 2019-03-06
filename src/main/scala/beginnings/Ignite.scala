36601353846152fe8047fb4f94dbbe5c9fd0640c
VenkataWunnava <venkatawunnava@gmail.com>
2019-03-06 17:48:47 -0500

    version2

--- src/main/scala/beginnings/Ignite.scala
+++ src/main/scala/beginnings/Ignite.scala
@@ -2,6 +2,11 @@ package beginnings
 import org.apache.spark.sql.SparkSession
 
 //YARN Client mode deploy from IDE
+//Add SPARK_HOME to Run configurations -> Env Variables: /<path to spark jars>/spark-2.3.2-bin-hadoop2.7
+//https://community.hortonworks.com/questions/84654/how-to-run-spark-application-with-yarn-client-mode.html
+
+case class Team (team:String, eliminated:String, origin: String, colors: String)
+
 object Ignite {
 
   def main(args: Array[String]) {
@@ -15,10 +20,27 @@ object Ignite {
       .config("spark.driver.extraJavaOptions","-Dhdp.version=3.1.0.0-78")
       .config("spark.yarn.am.extraJavaOptions","-Dhdp.version=3.1.0.0-78")
       .config("spark.jars", "/Users/vwunnava/Documents/HWProjects/P72/BitBucket/spark_playdoe/target/spark_playdoe-1.0-SNAPSHOT.jar")
+      .config("spark.eventLog.enabled","true")
+      .config("spark.eventLog.dir","hdfs://alpha.hdp.com:8020/spark2-history/")
       .getOrCreate()
+
+
+   //local mode
+/*    val sparkS = SparkSession.builder.appName("Spark-HW12622-Configurations")
+      .master("yarn")
+      .config("spark.driver.memory","1g")
+      .config("spark.submit.deployMode","client")
+      .getOrCreate()*/
 
     // Load the text into a Spark RDD, which is a distributed representation of each line of text
     val textFile = sparkS.sparkContext.textFile("src/main/resources/somefile.xml")
+
+/*    val myFistDF = sparkS.range(10,100)
+    val myJsonDF = sparkS.read.json("file:///Users/vwunnava/Documents/HWProjects/P72/BitBucket/spark_playdoe/src/main/resources/jsonRecords.json")
+    println(myJsonDF.printSchema())
+    myJsonDF.collect().foreach(println)
+    val myJsonDS = myJsonDF.as[Team]
+    myJsonDS.show()*/
 
     //word count
     val counts = textFile.flatMap(line => line.split(" "))
@@ -27,6 +49,11 @@ object Ignite {
 
     counts.foreach(println)
     System.out.println("Total words: " + counts.count());
-    counts.saveAsTextFile("/tmp/somefile3.xml")
+
+    //use file prefix to store in local
+    counts.saveAsTextFile("file:///tmp/somefile4.xml")
+
+
+
   }
 }
