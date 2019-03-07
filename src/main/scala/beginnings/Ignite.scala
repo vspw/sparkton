9e1b7d6a40ebdf4e414276644812a0d601623da0
VenkataWunnava <venkatawunnava@gmail.com>
2019-03-07 15:38:09 -0500

    version3-updatedWithConfigsForSquadron

--- src/main/scala/beginnings/Ignite.scala
+++ src/main/scala/beginnings/Ignite.scala
@@ -1,9 +1,19 @@
 package beginnings
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SparkSession
+import java.util.Properties
+import java.io.FileInputStream
 
 //YARN Client mode deploy from IDE
 //Add SPARK_HOME to Run configurations -> Env Variables: /<path to spark jars>/spark-2.3.2-bin-hadoop2.7
 //https://community.hortonworks.com/questions/84654/how-to-run-spark-application-with-yarn-client-mode.html
+//add hdp.version in yarn site OR add SPARK_JAVA_OPTS="-Dhdp.version=3.1.0.0-78"
+//the yarn cluster should be able to connect to driver
+/*/usr/hdp/current/spark2-client/bin/spark-submit --class beginnings.Ignite --master yarn-client
+--num-executors 1 --driver-memory 512m --executor-memory 512m --executor-cores 1  ./spark_playdoe-1.0-SNAPSHOT.jar*/
+//create the output directory in hdfs
+//update project module settings to have the correct resource folder
+//try adding the spark-jars.zip to an archive directory in the hdfs cluster
 
 case class Team (team:String, eliminated:String, origin: String, colors: String)
 
@@ -13,17 +23,41 @@ object Ignite {
    val props = System.getProperties
     System.setProperty("HADOOP_USER_NAME", "root")
     println(System.getenv("SPARK_HOME"))
+   println(System.getenv("SPARK_CONF_DIR"))
     props.entrySet().toArray.foreach(println)
-    val sparkS = SparkSession.builder.appName("Spark-HW12622-Configurations")
-      .master("yarn").config("spark.driver.memory","1g")
-      .config("spark.submit.deployMode","client")
+
+
+    val confHouse: SparkConf = new SparkConf()
+     .setMaster("yarn")
+     .setAppName("spark-app-123")
+     .set("spark.submit.deployMode","client")
+     .set("spark.eventLog.dir","hdfs://alpha.hdp.com:8020/spark2-history/")
+     .set("spark.eventLog.enabled","true")
+     .set("spark.jars", "/Users/vwunnava/Documents/HWProjects/P72/BitBucket/spark_playdoe/target/spark_playdoe-1.0-SNAPSHOT.jar")
+     .set("spark.yarn.archive","hdfs://alpha.hdp.com:8020/user/spark/spark-jars.zip")
+     .set("spark.hadoop.yarn.resourcemanager.hostname","alpha.hdp.com")
+     .set("spark.hadoop.yarn.resourcemanager.address","alpha.hdp.com:8050")
+     .set("spark.yarn.access.namenodes", "hdfs://alpha.hdp.com:8020")
+     .set("spark.yarn.stagingDir", "hdfs://alpha.hdp.com:8020/user/spark/tmp/")
+      .set("spark.driver.extraJavaOptions","-Dhdp.version=3.1.0.0-78")
+      .set("spark.yarn.am.extraJavaOptions","-Dhdp.version=3.1.0.0-78")
+
+
+    val confSquadron: SparkConf = new SparkConf()
+     .setMaster("yarn")
+     .setAppName("spark-app-123")
+     .set("spark.submit.deployMode","client")
+     .set("spark.eventLog.dir","hdfs://hostname:8020/spark2-history/")
+     .set("spark.eventLog.enabled","true")
+     .set("spark.yarn.archive","hdfs://hostname:8020/user/spark/spark-jars.zip")
+
+
+    val sparkS = SparkSession.builder.config(confSquadron).getOrCreate()
+/*
       .config("spark.driver.extraJavaOptions","-Dhdp.version=3.1.0.0-78")
       .config("spark.yarn.am.extraJavaOptions","-Dhdp.version=3.1.0.0-78")
       .config("spark.jars", "/Users/vwunnava/Documents/HWProjects/P72/BitBucket/spark_playdoe/target/spark_playdoe-1.0-SNAPSHOT.jar")
-      .config("spark.eventLog.enabled","true")
-      .config("spark.eventLog.dir","hdfs://alpha.hdp.com:8020/spark2-history/")
-      .getOrCreate()
-
+*/
 
    //local mode
 /*    val sparkS = SparkSession.builder.appName("Spark-HW12622-Configurations")
@@ -51,7 +85,7 @@ object Ignite {
     System.out.println("Total words: " + counts.count());
 
     //use file prefix to store in local
-    counts.saveAsTextFile("file:///tmp/somefile4.xml")
+    counts.saveAsTextFile("file:///tmp/somefile5.xml")
 
 
 
